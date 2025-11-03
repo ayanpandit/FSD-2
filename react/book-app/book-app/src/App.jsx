b8191f2371b9a6be5df38184a5aabdc22c15a09d
Ayan Pandey <aayanpandey8528@gmail.com>
2025-11-03 12:50:17 +0530

    added the one book

---
+++ react/book-app/book-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Book from './component/book'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Book />
+    </div>
+  )
+}
+
+export default App
---
+++ react/book-app/book-app/src/component/book.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./book.css"
+function Book ()
+{
+    return (
+        <div>
+            <img src="" alt="" />
+            <h1>Book Title</h1>
+            <h2>250 </h2>
+        </div>
+    )
+}
+export default Book;
