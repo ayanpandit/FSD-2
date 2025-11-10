d13e5caebbeef3057f0cbceb237cf34fa4647982
Ayan Pandey <aayanpandey8528@gmail.com>
2025-11-10 11:55:03 +0530

    added the book.jsx

--- react/book-app/book-app/src/component/book.jsx
+++ react/book-app/book-app/src/component/book.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import "./book.css"
 function Book ()
 {
+    // Use a random book cover image from Open Library
+    const imgSrc = `https://covers.openlibrary.org/b/id/${Math.floor(7000000 + Math.random()*2000000)}-L.jpg`;
     return (
-        <div>
-            <img src="" alt="" />
+        <>
+            <img src={imgSrc} alt="Random Book Cover" style={{ width: 120, height: 180, objectFit: 'cover', borderRadius: 8 }} />
             <h1>Book Title</h1>
-            <h2>250 </h2>
-        </div>
+            <h2>250</h2>
+        </>
     )
 }
 export default Book;
